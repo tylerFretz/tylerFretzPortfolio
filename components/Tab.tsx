d9c938048694877c368f4e179d1f2fe20a4136b2
DESKTOP-K20A7N9\fretz <fretztyler@gmail.com>
2023-01-02 14:49:53 -0500

    revert tab close

---
+++ components/Tab.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../styles/Tab.module.css";
+
+interface TabProps {
+  icon: string;
+  filename: string;
+  path: string;
+}
+
+const Tab = ({ icon, filename, path }: TabProps) => {
+  const { pathname } = useRouter();
+
+  return (
+      <Link href={path} className={styles.link}>
+        <div className={`${styles.tab} ${pathname === path && styles.active}`}>
+          <Image src={icon} alt={filename} height={18} width={18} />
+          <p>{filename}</p>
+        </div>
+      </Link>
+  );
+};
+
+export default Tab;
---
+++ components/TabsBar.tsx
@@ -0,0 +1,52 @@
+import styles from "../styles/Tabsbar.module.css";
+import Tab from "./Tab";
+
+const tabs = [
+  {
+    name: "home.tsx",
+    path: "/",
+    icon: "/react_icon.svg",
+  },
+  {
+    name: "about.ts",
+    path: "/about",
+    icon: "/typescript_icon.svg",
+  },
+  {
+    name: "contact.css",
+    path: "/contact",
+    icon: "/css_icon.svg",
+  },
+  {
+    name: "projects.cs",
+    path: "/projects",
+    icon: "/csharp_icon.svg",
+  },
+  {
+    name: "github.md",
+    path: "/github",
+    icon: "/markdown_icon.svg",
+  },
+  {
+    name: "settings.json",
+    path: "/settings",
+    icon: "/json_icon.svg",
+  },
+];
+
+const Tabsbar = () => {
+  return (
+    <div className={styles.tabs}>
+      {tabs.map((tab) => (
+        <Tab
+          key={tab.name}
+          icon={tab.icon}
+          filename={tab.name}
+          path={tab.path}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Tabsbar;

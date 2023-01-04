d804a620796e66bb559e194d5fdb876f1920f4e4
DESKTOP-K20A7N9\fretz <fretztyler@gmail.com>
2023-01-04 14:20:29 -0500

    Versioning untracked files

--- components/Explorer.tsx
+++ components/Explorer.tsx
@@ -1,4 +1,4 @@
-import ChevronRightIcon from "@/components/icons/ChevronRightIcon";
+import ChevronRightIcon from "@/icons/ChevronRightIcon";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
